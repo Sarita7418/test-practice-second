a7569130fdd6f70f6d2929b1c1a0ff6cee352f16
Sarita731 <mg6667418@gmail.com>
2024-10-18 22:30:18 -0400

    middle

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import {getProfiles} from "./lib/getProfiles";
+import Profile from "./components/Profile";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [profiles, setprofiles] = useState([]);
+  useEffect(() => {
+    async function fetchdata() {
+      try {
+        const response = await getProfiles("https://670023414da5bd2375534f66.mockapi.io/api/v1/test");
+        setprofiles(response);
+        console.log(profiles);
+      } catch (error) {
+        console.log("Error al pedir");
+      }
+    }
+    fetchdata();
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  <>
+    <section>
+      {profiles.map((profile) => (
+        <Profile key={profile.id} profile={profile}/>
+      ))}
+    </section>
+  </>
+  );
 }
 
-export default App
+export default App;
